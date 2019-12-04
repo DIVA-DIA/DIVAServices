3053e07a62e05a22bfbc24432cc74eba570bf9d2
Marcel Würsch <marcel.wuersch@unifr.ch>
2019-12-04 15:06:09 +0100

    add response when workflow is created
    - check that the same workflow can't be created twice

--- src/workflows/workflowManager.ts
+++ src/workflows/workflowManager.ts
@@ -233,7 +233,7 @@ export class WorkflowManager {
             version: this.version,
             status: {
                 statusCode: 200,
-                statusMessage: "This workflow is available"
+                statusMessage: "Started Workflow Creation"
             },
             statistics: {
                 runtime: -1,
@@ -579,6 +579,7 @@ export class WorkflowManager {
     }
 
     public getStatus() : any {
-        return AlgorithmManagement.getStatusByRoute(this.route)
+        let route = this.route.split('/').slice(0,-1).join('/');
+        return AlgorithmManagement.getStatusByRoute('/' +  route)
     }
 }
